f6244ddd77881cd78f5ffd2ba93ce7607209b46d
Ryan Connolly <rconnolly.work@outlook.com>
2019-05-28 15:27:06 -0400

    fixed nav

--- Client/wwwroot/auth0-app/src/app/nav/nav.component.ts
+++ Client/wwwroot/auth0-app/src/app/nav/nav.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
+import { map, filter } from 'rxjs/operators';
+import { MatSidenav } from '@angular/material';
+
+
 
 @Component({
   selector: 'app-nav',
@@ -10,11 +14,38 @@ import { map } from 'rxjs/operators';
 })
 export class NavComponent {
 
+  @ViewChild(MatSidenav) drawer: MatSidenav;
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+    watcher: Subscription;
+    isMobile: Boolean;
 
+    constructor(private breakpointObserver: BreakpointObserver, mediaObserver: MediaObserver) {
+  
+      this.watcher = mediaObserver.asObservable()
+      .pipe(
+        filter((changes: MediaChange[]) => changes.length > 0),
+        map((changes: MediaChange[]) => changes[0])
+      ).subscribe((change: MediaChange) => {
+        //this.activeMediaQuery = change ? `'${change.mqAlias}' = (${change.mediaQuery})` : '';
+        if ( change.mqAlias == 'sm' || change.mqAlias == 'xs') {
+          this.isMobile = true;
+        }
+        else{
+          this.isMobile = false;
+          this.drawer.open();
+        }
+      });
+  
+    }
+
+    toggleDrawer(){
+      if(this.isMobile){
+        this.drawer.toggle();
+      }     
+    }
 }
